91b4ebd9a6cceae46a107b9b0d50de55ce6100c0
sky <skywong0571@gmail.com>
2025-05-17 03:23:14 +0800

    feat:脚手架搭建

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,14 @@
+import './assets/main.css'
+
 import { createApp } from 'vue'
-import './style.css'
+import { createPinia } from 'pinia'
+
 import App from './App.vue'
 import router from './router'
-import { createStore } from 'vuex'
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
-
-// 创建一个新的store实例
-const store = createStore({
-  state() {
-    return {
-      count: 0
-    }
-  },
-  mutations: {
-    increment(state) {
-      state.count++
-    }
-  }
-})
 
 const app = createApp(App)
 
-app.use(store).use(ElementPlus).use(router).mount('#app')
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,31 +1,23 @@
-/*
- * @Description  :
- * @Author       : Sky
- * @Date         : 2022-12-08 11:27:49
- */
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
 
-import { createRouter, createWebHashHistory } from 'vue-router'
-import Home from '../page/home.vue'
-import About from '../page/about/about.vue'
-
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    component: About
-  }
-]
-
-// createRouter用来新建路由实例
-// createWebHashHistor用来配置使用hash模式
 const router = createRouter({
-  history: createWebHashHistory(),
-  routes
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView,
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/AboutView.vue'),
+    },
+  ],
 })
 
 export default router
