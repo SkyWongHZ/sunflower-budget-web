c837025a7790301a392d6009472582399177fb36
sky <skywong0571@gmail.com>
2025-05-17 04:59:54 +0800

    feat:less语法支持

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
-import './assets/main.css'
+import './assets/styles/global.less'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import Vant from 'vant'
+import 'vant/lib/index.css'
 
 import App from './App.vue'
 import router from './router'
@@ -10,5 +12,6 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(Vant)
 
 app.mount('#app')
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,24 @@
 <script setup lang="ts">
 import ViewportDemo from '../components/ViewportDemo.vue'
+import LessDemo from '../components/LessDemo.vue'
 </script>
 
 <template>
   <main>
-    <ViewportDemo />
+    <LessDemo />
+    <!-- <ViewportDemo /> -->
   </main>
 </template>
+
+<style lang="less" scoped>
+@import '../assets/styles/variables.less';
+
+main {
+  max-width: 800px;
+  margin: 0 auto;
+  padding: @padding-md;
+  background-color: @white;
+  border-radius: @border-radius-md;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
+}
+</style>
