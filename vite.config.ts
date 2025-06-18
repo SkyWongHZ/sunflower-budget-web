897463eb1eb544720646136bc6bdb623204a4673
sky <skywong0571@gmail.com>
2025-06-18 17:40:07 +0800

    feat:uni-app框架配置修改

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import uni from "@dcloudio/vite-plugin-uni";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [uni()],
+  server: {
+    port: 3000, // 修改为你想要的端口号
+    host: '0.0.0.0', // 允许外部访问
+    open: true // 自动打开浏览器
+  }
 });
